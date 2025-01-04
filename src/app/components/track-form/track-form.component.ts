7d93095c27b4047160a809802eb94d366e6e6aa8
asmaabarj <asmaabarj5@gmail.com>
2025-01-04 21:22:16 +0000

    la structure du projet

---
+++ src/app/components/track-form/track-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './track-form.component.html',
+  styleUrl: './track-form.component.css'
+})
+export class TrackFormComponent {
+
+}
